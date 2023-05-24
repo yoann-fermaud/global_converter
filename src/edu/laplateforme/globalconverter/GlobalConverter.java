40cfa1590807ca98f3131d3b300ef31888d2ff43
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-05-24 16:19:00 +0200

    [ADD] regex and new error message

--- src/edu/laplateforme/globalconverter/GlobalConverter.java
+++ src/edu/laplateforme/globalconverter/GlobalConverter.java
@@ -5,11 +5,28 @@ import edu.laplateforme.globalconverter.common.CommandLineOptionsHandler;
 
 public class GlobalConverter {
     public static void main(String[] args) {
-        if (1 < args.length) {
+        if (1 < args.length && validChar(args[1])) {
             CommandLineOptionsHandler.optionArg(args[0], args[1]);
         } else {
-            System.out.println("Invalid command line !");
-            System.out.println("$ java GlobalConverter [OPTION] YOUR_STRING");
+            System.out.println("Invalid command line and/or alphanumeric character only !");
+            System.out.println("""
+                    NAME
+                    \t$ java GlobalConverter [OPTION] YOUR_STRING
+                    
+                    SYNOPSIS
+                    \t$ java GlobalConverter -b|binary [OPTION OF GlobalConverter]
+                    \t$ java GlobalConverter -d|decimal [OPTION OF GlobalConverter]
+                    \t$ java GlobalConverter -h|hexadecimal [OPTION OF GlobalConverter]
+                    \t$ java GlobalConverter -o|octal [OPTION OF GlobalConverter]
+                    \t$ java GlobalConverter -t|text [OPTION OF GlobalConverter]
+                    """);
         }
+    }
+
+    private static boolean validChar(String string) {
+        // Regular expression to check alphanumeric characters with spaces
+        String regex = "^[a-zA-Z0-9 ]+$";
+        // Check if the string matches the regular expression
+        return string.matches(regex);
     }
 }
